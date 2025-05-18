d4c188c0c4c5e0ce4c1e7158fd535146d3c1cb7e
Sergio Cuadros <sergiocuadros44@gmail.com>
2025-05-17 23:33:23 -0600

    add seting

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['framer-motion']
-    }
+      external: [], // Asegúrate de no tener 'framer-motion' como externo
+    },
   },
-  plugins: [react()],
-  
-})
+});
